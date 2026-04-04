6d8f89419e1e825f23dae5056329b9ea183cb96e
Marko <nakicmarko555@gmail.com>
2026-04-04 21:43:31 +0200

    feat(nvim-funk): add zen toggles and file utility keys

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -30,6 +30,7 @@ vim.keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "New tab" })
 vim.keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close tab" })
 vim.keymap.set("n", "<leader>tn", "<cmd>tabnext<CR>", { desc = "Next tab" })
 vim.keymap.set("n", "<leader>tp", "<cmd>tabprevious<CR>", { desc = "Previous tab" })
+vim.keymap.set("n", "<leader>tf", "<cmd>tab split<CR>", { desc = "Current buffer in new tab" })
 
 vim.keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split vertical" })
 vim.keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split horizontal" })
@@ -38,6 +39,33 @@ vim.keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close split" })
 vim.keymap.set("n", "<leader>th", function()
   require("nvchad.themes").open()
 end, { desc = "Theme switcher" })
+
+vim.keymap.set("n", "<leader>x", function()
+  local path = vim.api.nvim_buf_get_name(0)
+  if path == "" then
+    vim.notify("No file associated with the current buffer", vim.log.levels.WARN)
+    return
+  end
+
+  vim.fn.system({ "chmod", "+x", path })
+  if vim.v.shell_error ~= 0 then
+    vim.notify("Failed to make file executable: " .. vim.fn.fnamemodify(path, ":~"), vim.log.levels.ERROR)
+    return
+  end
+
+  vim.notify("Made executable: " .. vim.fn.fnamemodify(path, ":~"))
+end, { desc = "Make current file executable" })
+
+vim.keymap.set("n", "<leader>fp", function()
+  local path = vim.fn.expand("%:~")
+  if path == "" then
+    vim.notify("No file associated with the current buffer", vim.log.levels.WARN)
+    return
+  end
+
+  vim.fn.setreg("+", path)
+  vim.notify("Copied file path: " .. path)
+end, { desc = "Copy file path" })
 
 vim.keymap.set({ "n", "t" }, "<A-t>", function()
   Snacks.terminal.toggle(nil, {
--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -4,9 +4,31 @@ return {
     main = "snacks",
     lazy = false,
     priority = 1000,
+    keys = {
+      {
+        "<leader>bd",
+        function()
+          Snacks.bufdelete()
+        end,
+        desc = "Delete buffer",
+      },
+      {
+        "<leader>bo",
+        function()
+          Snacks.bufdelete.other()
+        end,
+        desc = "Delete other buffers",
+      },
+    },
     opts = {
       lazygit = {},
+      toggle = {},
       terminal = {},
+      zen = {
+        toggles = {
+          dim = false,
+        },
+      },
       styles = {
         lazygit = {
           width = 0.96,
@@ -25,5 +47,40 @@ return {
         },
       },
     },
+    config = function(_, opts)
+      local Snacks = require("snacks")
+
+      Snacks.setup(opts)
+      Snacks.toggle.zen():map("<leader>zm", { desc = "Zen mode" })
+      Snacks.toggle.zoom():map("<leader>sz", { desc = "Zoom split" })
+      Snacks.toggle.diagnostics():map("<leader>ud", { desc = "Diagnostics" })
+      Snacks.toggle.inlay_hints():map("<leader>uh", { desc = "Inlay hints" })
+      Snacks.toggle.option("wrap", { name = "Wrap" }):map("<leader>uw")
+      Snacks.toggle.option("spell", { name = "Spelling" }):map("<leader>us")
+      Snacks.toggle
+        .new({
+          id = "global_autoformat",
+          name = "Global Auto Format",
+          get = function()
+            return not vim.g.disable_autoformat
+          end,
+          set = function(state)
+            vim.g.disable_autoformat = not state
+          end,
+        })
+        :map("<leader>uf")
+      Snacks.toggle
+        .new({
+          id = "buffer_autoformat",
+          name = "Buffer Auto Format",
+          get = function()
+            return not vim.b.disable_autoformat
+          end,
+          set = function(state)
+            vim.b.disable_autoformat = not state
+          end,
+        })
+        :map("<leader>uF")
+    end,
   },
 }
