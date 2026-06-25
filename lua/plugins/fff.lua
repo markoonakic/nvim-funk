8ea9fdb9e33e0aafe988badcca91f987ec0b577c
Marko <nakicmarko555@gmail.com>
2026-06-25 13:47:41 +0200

    fix(fff): use nix build hook on NixOS

--- lua/plugins/fff.lua
+++ lua/plugins/fff.lua
@@ -1,7 +1,9 @@
+local is_nixos = vim.env.NVIM_MASON_AUTO_INSTALL == "0"
+
 return {
   "dmtrKovalenko/fff.nvim",
   lazy = false,
-  build = function()
+  build = is_nixos and "nix run .#release" or function()
     require("fff.download").download_or_build_binary()
   end,
   opts = {},
